9a737406b23fd369e3a9427e040156261147e04a
kylebrackman <kyle.a.brackman@gmail.com>
2023-04-16 19:55:19 -0600

    trying to fix rendering bug

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,30 +1,19 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import NavBar from "./Navbar";
+import '../App.css';
+import { Route, Routes } from 'react-router-dom'
+import { UserProvider } from '../context/user';
+import Navbar from './Navbar'
+import Home from './Home';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
-
   return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar />
-        <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            <h1>Page Count: {count}</h1>
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <UserProvider>
+        <Navbar />
+        <Routes>
+          <Route exact path="/home" element={<Home />} />
+        </Routes>
+      </UserProvider>
+    </div>
   );
 }
 
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { UserContext } from "../Context/user";
+import { UserContext } from "../context/user";
 import { NavLink, useNavigate } from 'react-router-dom'
+import Home from "./Home";
 
 const NavBar = () => {
     const { user, logout, loggedIn } = useContext(UserContext)
@@ -19,29 +20,11 @@ const NavBar = () => {
     }
 
     // had to add the && user for this to work...?
-    if (loggedIn && user) {
-        return (
-            <div>
-                <h1>Beta Spray</h1>
-                <NavLink to='/home'>
-                    <button> Home Page </button>
-                </NavLink>
-                <button onClick={logoutUser}>Logout</button>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <NavLink to='/login'>
-                    <button>Login</button>
-                </NavLink >
-                <NavLink to='/signup'>
-                    <button>Signup</button>
-                </NavLink>
-                <hr />
-            </div>
-        )
-    }
+    return (
+        <div>
+            test
+        </div>
+    )
 }
 
 export default NavBar
