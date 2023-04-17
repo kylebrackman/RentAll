83cb1e64ebefe7e25c1f2145e94eb570f2241c68
kylebrackman <kyle.a.brackman@gmail.com>
2023-04-16 18:32:00 -0600

    added sessions controller and custom routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+    def create
+
+    end
+
+    def destroy
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :rentals
   resources :items
   resources :users
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  
   get '/hello', to: 'application#hello_world'
 
   get '*path',
