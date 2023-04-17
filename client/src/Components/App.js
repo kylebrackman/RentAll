d2ad616cfbb66f31c916ec6684021d1cf9f1c995
kylebrackman <kyle.a.brackman@gmail.com>
2023-04-17 14:47:45 -0600

    added login and signup files

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import { UserProvider } from '../context/user';
 import Navbar from './Navbar'
 import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </UserProvider>
     </div>
---
+++ client/src/Components/Login.js
@@ -0,0 +1,60 @@
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../context/user';
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
+    const { login } = useContext(UserContext)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+            .then(res => res.json())
+            .then((user) => {
+                if (!user.error) {
+                    login(user)
+                    navigate('/home')
+                } else {
+                    setUsername("")
+                    setPassword("")
+                    const errorLi = <li>{user.error}</li>
+                    setError(errorLi)
+                }
+            })
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Username: </label>
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                /> <br />
+                <label>Password: </label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                /> <br />
+                <input type="submit" />
+            </form>
+            <>
+                {error}
+            </>
+        </div>
+    )
+}
+
+export default Login
