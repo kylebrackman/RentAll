55d559b5ff94cd66fbfceb6a4972c88fcf007669
kylebrackman <kyle.a.brackman@gmail.com>
2023-05-10 20:09:07 -0600

    added no overlap validation for rental dates

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,11 +2,20 @@ class Rental < ApplicationRecord
 
     validate :start_date
     validate :end_date
+    validate :no_overlapping_rentals
 
     belongs_to :renter, class_name: "User"
     belongs_to :item, class_name: "Item"
     
     def self.current_rentals(user)
         where(renter_id: user.id).where("start_date >= ? AND end_date <= ? ", Date.today, Date.today)
+    end
+
+    def no_overlapping_rentals
+        existing_rentals = Rental.where(item_id: item_id).where("start_date <= ? AND end_date >= ?", start_date, end_date)
+
+        if existing_rentals.exists?
+            errors.add(:start_date, "cannot overlap with an existing rental")
+        end
     end
 end
