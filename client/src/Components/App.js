37702a9405f59b97e394b215e0906e8bede28cb8
kylebrackman <kyle.a.brackman@gmail.com>
2023-07-08 14:15:18 -0600

    fixed app routing error

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -20,7 +20,7 @@ function App() {
       <UserProvider>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Signup />} />
+          <Route path='/' element={<AllItems />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/uploaditem" element={<UploadItem />} />
