2e520ceda65e090161dec66860953ee613af4a68
kylebrackman <kyle.a.brackman@gmail.com>
2023-07-11 14:05:08 -0600

    added validation for password minimum length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+\z/, message: "Invalid email format" }
+    validates :password, presence: true, length: { minimum: 8 }
 
     has_secure_password 
     
