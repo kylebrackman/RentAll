d1ae8ad8b8518d43b2f4d50d6aa2475fa854a9dd
Kyle Brackman <kyle@Kyles-MacBook-Pro.local>
2023-11-21 13:33:08 -0700

    cloned RentAll to computer

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_045601) do
     t.datetime "updated_at", null: false
     t.integer "renter_id"
     t.integer "item_id"
-    t.date "start_date", default: "2023-05-13"
+    t.date "start_date", default: "2023-11-21"
     t.date "end_date"
     t.integer "owner_id"
   end
