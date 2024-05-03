c3284bbb5a56e4a3ae250cd7092031b4b1cff642
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-05-03 12:35:54 -0600

    Fix spacing on signup form.

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -55,7 +55,7 @@ function Signup() {
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Create an account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                        <form class="space-y-4 md:space-y-2" onSubmit={handleSubmit}>
                             <div>
                                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >Email</label>
                                 <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
